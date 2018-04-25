345ef96e6ec989090a1da952692655d20de8403a
davidkurnia--tunaiku <david.kurnia@amarbank.co.id>
2018-04-25 16:09:35 +0700

    Removed create-react-app generated files. Updated version.

--- docs/src/index.js
+++ docs/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(<App />, rootElement);
 registerServiceWorker();
