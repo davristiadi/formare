109139e70325c8af0d472fdca30320db2b11669f
davidkurnia--tunaiku <david.kurnia@amarbank.co.id>
2018-04-25 17:49:03 +0700

    Added example content in /docs.

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+	componentDidMount = () => {
+		document.title = 'Formare | Hassle-free CSS Framework to build web interfaces easily.';
+	}
+	
 	render() {
 		return (
 			<div id="home">
-			
+				<section id="hero" className="hero hero--is-primary">
+				</section>
+				<section id="card" className="padding-top-is-x-large padding-bottom-is-x-large">
+					<div className="container">
+						<article className="article">
+							<div className="heading">
+								<h3 className="font-is-bold">Card</h3>
+								<p>Culpa culpa deserunt magna nostrud sunt adipisicing cillum commodo dolore cupidatat nostrud veniam. Commodo id nulla consectetur aliqua aute. Fugiat irure consectetur cillum pariatur laboris est.</p>
+							</div>
+							<div className="row">
+								<div className="column">
+									<div className="card card--is-dark">
+										<div className="card__header">
+											<span className="font-is-x-large font-is-bold">This is a card</span>
+										</div>
+										<div className="card__body">
+											<article className="article">
+												<h5 className="font-is-medium">This is an example card content</h5>
+												<p>Proident et quis sunt enim adipisicing consequat nulla do quis. Ipsum esse fugiat elit est cillum id laboris ullamco sint qui. Eu tempor sint sit esse mollit ea aliquip elit non. Sint ut esse ea deserunt velit incididunt sint laborum dolor duis occaecat minim. Et eu consequat proident incididunt minim nostrud eiusmod reprehenderit mollit do deserunt ad nisi. Nisi ea dolor proident consequat mollit est aliqua ullamco sit exercitation elit do deserunt Lorem.</p>
+											</article>
+										</div>
+										<div className="card__footer flex justify-content--end">
+											<button className="button button--is-danger margin-right">Cancel</button>
+											<button className="button button--is-primary">Submit</button>
+										</div>
+									</div>
+								</div>
+								<div className="column">
+									<div className="card card--is-lighter">
+										<div className="card__header">
+											<span className="font-is-x-large font-is-bold">This is a card</span>
+										</div>
+										<div className="card__body">
+											<article className="article">
+												<h5 className="font-is-medium">This is an example card content</h5>
+												<p>Aliquip elit proident excepteur esse in do sunt fugiat velit cillum voluptate. Lorem ad ullamco ad officia ea laboris veniam nulla nisi ullamco esse mollit excepteur ex. Eiusmod Lorem voluptate cillum anim amet laboris est do do exercitation eu incididunt commodo. Magna deserunt eu enim reprehenderit officia aute occaecat elit adipisicing et laboris.</p>
+											</article>
+										</div>
+										<div className="card__footer flex justify-content--end">
+											<button className="button button--is-danger button--is-outlined margin-right">Cancel</button>
+											<button className="button button--is-primary button--is-outlined">Submit</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</article>
+					</div>
+				</section>
 			</div>
 		);
 	}
