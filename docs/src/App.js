67c7ed0814ff703032b34b053ec7ab0d6c2e9da4
davidkurnia--tunaiku <david.kurnia@amarbank.co.id>
2018-04-25 18:45:42 +0700

    Updated version to 0.1.2

--- docs/src/App.js
+++ docs/src/App.js
@@ -8,14 +8,52 @@ class App extends Component {
 	render() {
 		return (
 			<div id="home">
+				<div className="navbar navbar--is-lighter">
+					<div className="navbar__container">
+						<div className="navbar__block navbar__block--flex-start">
+							<h5 className="font-is-bold">Formare</h5>
+						</div>
+						<div className="navbar__block navbar__block--flex-end">
+							<nav className="nav">
+								<ul className="nav__list">
+									<li className="nav__item">
+										<a className="nav__link">Home</a>
+									</li>
+									<li className="nav__item">
+										<a className="nav__link">Documentation</a>
+									</li>
+									<li className="nav__item">
+										<a className="nav__link">Help</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
 				<section id="hero" className="hero hero--is-primary">
 				</section>
-				<section id="card" className="padding-top-is-x-large padding-bottom-is-x-large">
+				<section id="heading" className="padding-top-is-x-large">
 					<div className="container">
 						<article className="article">
 							<div className="heading">
-								<h3 className="font-is-bold">Card</h3>
-								<p>Culpa culpa deserunt magna nostrud sunt adipisicing cillum commodo dolore cupidatat nostrud veniam. Commodo id nulla consectetur aliqua aute. Fugiat irure consectetur cillum pariatur laboris est.</p>
+								<h3 className="title">Heading</h3>
+								<p className="subtitle">Quis amet deserunt qui culpa velit ut culpa et voluptate in consequat ullamco et laboris.</p>
+							</div>
+							<h1>Heading 1</h1>
+							<h2>Heading 2</h2>
+							<h3>Heading 3</h3>
+							<h4>Heading 4</h4>
+							<h5>Heading 5</h5>
+							<h6>Heading 6</h6>
+						</article>
+					</div>
+				</section>
+				<section id="card" className="padding-top-is-x-large">
+					<div className="container">
+						<article className="article">
+							<div className="heading">
+								<h3 className="title">Card</h3>
+								<p className="subtitle">Culpa culpa deserunt magna nostrud sunt adipisicing cillum commodo dolore cupidatat nostrud veniam. Commodo id nulla consectetur aliqua aute. Fugiat irure consectetur cillum pariatur laboris est.</p>
 							</div>
 							<div className="row">
 								<div className="column">
