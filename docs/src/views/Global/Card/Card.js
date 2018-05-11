e7731874db6195ae31446ee3f7960fc60657e3b1
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2018-05-12 00:32:54 +0700

    Fixed an issue where card--has-overlay rule would iterate non-stop

--- docs/src/views/Global/Card/Card.js
+++ docs/src/views/Global/Card/Card.js
@@ -14,7 +14,8 @@ const Card = props => {
                         <div className="grid grid-6 mg-bottom">
                             <div className="card">
                                 <div className="card__header">
-                                    <span className="fs-large fw-bold">Card Title</span>
+                                    <h4 className="title">Card Title</h4>
+                                    <h5 className="subtitle color-gray">This is a subtitle.</h5>
                                 </div>
                                 <div className="card__body">
                                     <span>Id anim officia commodo voluptate magna aliqua. Laborum pariatur ullamco occaecat labore aliqua et quis. Eiusmod reprehenderit pariatur laborum aute Lorem nisi et. Consectetur labore est consectetur officia. Laborum minim officia irure aliquip eu commodo aute. Quis culpa reprehenderit excepteur irure in. Est minim laborum consectetur nisi aliqua in sunt do veniam.</span>
@@ -26,9 +27,10 @@ const Card = props => {
                             </div>
                         </div>
                         <div className="grid grid-6 mg-bottom">
-                            <div className="card card--primary">
+                            <div className="card">
                                 <div className="card__header">
-                                    <span className="fs-large fw-bold">Card Title</span>
+                                    <h4 className="title">Card Title</h4>
+                                    <h5 className="subtitle color-gray">This is a subtitle.</h5>
                                 </div>
                                 <div className="card__body">
                                     <span>Id anim officia commodo voluptate magna aliqua. Laborum pariatur ullamco occaecat labore aliqua et quis. Eiusmod reprehenderit pariatur laborum aute Lorem nisi et. Consectetur labore est consectetur officia. Laborum minim officia irure aliquip eu commodo aute. Quis culpa reprehenderit excepteur irure in. Est minim laborum consectetur nisi aliqua in sunt do veniam.</span>
@@ -40,58 +42,64 @@ const Card = props => {
                             </div>
                         </div>
                         <div className="grid grid-6 mg-bottom">
-                            <div className="card card--secondary">
+                            <div className="card">
+                                <div className="card__image">
+                                    <img src="https://images.unsplash.com/photo-1519458246479-6acae7536988?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6c0202cc94df51412fc49471d653eee5&auto=format&fit=crop&w=1350&q=80" /> 
+                                </div>
                                 <div className="card__header">
-                                    <span className="fs-large fw-bold">Card Title</span>
+                                    <h4 className="title">Card Title</h4>
+                                    <h5 className="subtitle color-gray">This is a subtitle.</h5>
                                 </div>
                                 <div className="card__body">
-                                    <span>Id anim officia commodo voluptate magna aliqua. Laborum pariatur ullamco occaecat labore aliqua et quis. Eiusmod reprehenderit pariatur laborum aute Lorem nisi et. Consectetur labore est consectetur officia. Laborum minim officia irure aliquip eu commodo aute. Quis culpa reprehenderit excepteur irure in. Est minim laborum consectetur nisi aliqua in sunt do veniam.</span>
-                                </div>
-                                <div className="card__footer flex jc-end">
-                                    <button className="button button--danger mg-right">Cancel</button>
-                                    <button className="button button--success">Submit</button>
+                                    <article className="article">
+                                        {/* <div className="heading">
+                                            <h4 className="title">Card Title</h4>
+                                            <h5 className="subtitle color-gray">This is a subtitle.</h5>
+                                        </div> */}
+                                        <p>Tempor eiusmod excepteur esse culpa qui elit sint aliqua dolor. Qui mollit esse officia labore deserunt laborum dolore. Adipisicing mollit dolore eiusmod aute pariatur nulla culpa laborum culpa et dolore aute aute. Aute voluptate excepteur velit proident laboris qui.</p>
+                                    </article>
                                 </div>
                             </div>
                         </div>
                         <div className="grid grid-6 mg-bottom">
-                            <div className="card card--lighter">
+                            <div className="card">
+                                <div className="card__image">
+                                    <img src="https://images.unsplash.com/photo-1519458246479-6acae7536988?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6c0202cc94df51412fc49471d653eee5&auto=format&fit=crop&w=1350&q=80" /> 
+                                </div>
                                 <div className="card__header">
-                                    <span className="fs-large fw-bold">Card Title</span>
+                                    <h4 className="title">Card Title</h4>
+                                    <h5 className="subtitle color-gray">This is a subtitle.</h5>
                                 </div>
                                 <div className="card__body">
-                                    <span>Id anim officia commodo voluptate magna aliqua. Laborum pariatur ullamco occaecat labore aliqua et quis. Eiusmod reprehenderit pariatur laborum aute Lorem nisi et. Consectetur labore est consectetur officia. Laborum minim officia irure aliquip eu commodo aute. Quis culpa reprehenderit excepteur irure in. Est minim laborum consectetur nisi aliqua in sunt do veniam.</span>
-                                </div>
-                                <div className="card__footer flex jc-end">
-                                    <button className="button button--danger mg-right">Cancel</button>
-                                    <button className="button button--success">Submit</button>
+                                    <article className="article">
+                                        {/* <div className="heading">
+                                            <h4 className="title">Card Title</h4>
+                                            <h5 className="subtitle color-gray">This is a subtitle.</h5>
+                                        </div> */}
+                                        <p>Tempor eiusmod excepteur esse culpa qui elit sint aliqua dolor. Qui mollit esse officia labore deserunt laborum dolore. Adipisicing mollit dolore eiusmod aute pariatur nulla culpa laborum culpa et dolore aute aute. Aute voluptate excepteur velit proident laboris qui.</p>
+                                    </article>
                                 </div>
                             </div>
                         </div>
                         <div className="grid grid-6 mg-bottom">
-                            <div className="card card--light">
-                                <div className="card__header">
-                                    <span className="fs-large fw-bold">Card Title</span>
+                            <div className="card card--dark card--has-overlay">
+                                <div class="card__image">
+                                    <img src="https://images.unsplash.com/photo-1519458246479-6acae7536988?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6c0202cc94df51412fc49471d653eee5&auto=format&fit=crop&w=1350&q=80" />
                                 </div>
                                 <div className="card__body">
-                                    <span>Id anim officia commodo voluptate magna aliqua. Laborum pariatur ullamco occaecat labore aliqua et quis. Eiusmod reprehenderit pariatur laborum aute Lorem nisi et. Consectetur labore est consectetur officia. Laborum minim officia irure aliquip eu commodo aute. Quis culpa reprehenderit excepteur irure in. Est minim laborum consectetur nisi aliqua in sunt do veniam.</span>
-                                </div>
-                                <div className="card__footer flex jc-end">
-                                    <button className="button button--danger mg-right">Cancel</button>
-                                    <button className="button button--success">Submit</button>
+                                    <h5 class="title">Lorem ea non cillum.</h5>
+                                    <span>Cupidatat duis proident exercitation ullamco sunt labore.</span>
                                 </div>
                             </div>
                         </div>
                         <div className="grid grid-6 mg-bottom">
-                            <div className="card card--dark">
-                                <div className="card__header">
-                                    <span className="fs-large fw-bold">Card Title</span>
+                            <div className="card card--dark card--has-overlay">
+                                <div class="card__image">
+                                    <img src="https://images.unsplash.com/photo-1519458246479-6acae7536988?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6c0202cc94df51412fc49471d653eee5&auto=format&fit=crop&w=1350&q=80" />
                                 </div>
                                 <div className="card__body">
-                                    <span>Id anim officia commodo voluptate magna aliqua. Laborum pariatur ullamco occaecat labore aliqua et quis. Eiusmod reprehenderit pariatur laborum aute Lorem nisi et. Consectetur labore est consectetur officia. Laborum minim officia irure aliquip eu commodo aute. Quis culpa reprehenderit excepteur irure in. Est minim laborum consectetur nisi aliqua in sunt do veniam.</span>
-                                </div>
-                                <div className="card__footer flex jc-end">
-                                    <button className="button button--danger mg-right">Cancel</button>
-                                    <button className="button button--success">Submit</button>
+                                    <h5 class="title">Lorem ea non cillum.</h5>
+                                    <span>Cupidatat duis proident exercitation ullamco sunt labore.</span>
                                 </div>
                             </div>
                         </div>
