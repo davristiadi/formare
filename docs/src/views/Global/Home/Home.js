92d34d08274a5a2fb8fba49afb4f972a61c341d2
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2018-06-08 00:48:45 +0700

    Made a few adjustments in formare-docs. Divided each different card modifiers to its own separate sections to make them tidier.

--- docs/src/views/Global/Home/Home.js
+++ docs/src/views/Global/Home/Home.js
@@ -4,8 +4,8 @@ const Home = props => {
 	return (
 		<div id="home">
 			<section className="hero">
-				<div className="container">
-					<h2 className="title">Formare is lightweight, fast, and easy. </h2>
+				<div className="container pd-top-large">
+					<h2 className="title">Formare is lightweight, fast and easy to get started with. </h2>
 					<h4 className="subtitle color-gray">Veniam dolore adipisicing eu excepteur aliqua deserunt laboris dolor dolor commodo nisi occaecat aute pariatur sunt culpa mollit ullamco.</h4>
 				</div>
 			</section>
