8a77a4ac769c286402eeadc46086ec0b8648ae6e
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2018-06-08 18:28:56 +0700

    Added initial content in Navbar page

--- docs/src/views/Global/Docs/Components/Navbar/Navbar.js
+++ docs/src/views/Global/Docs/Components/Navbar/Navbar.js
@@ -11,7 +11,22 @@ const Navbar = props => {
                 <div class="fr-preview">
                     <div class="fr-preview__tag">Preview</div>
                     <div class="fr-preview__display">
-                        
+                        <div className="navbar">
+                        	<div className="navbar__container navbar__container--full-width">
+                        		<div className="navbar__block">
+                        			<h5>Logo</h5>
+                        		</div>
+                        		<div className="navbar__block flex jc-end">
+                        			<nav className="nav">
+                        				<ul className="nav__list">
+                        					<li className="nav__item"><a className="nav__link">Nav A</a></li>
+                        					<li className="nav__item"><a className="nav__link">Nav B</a></li>
+                        					<li className="nav__item"><a className="nav__link">Nav C</a></li>
+                        				</ul>
+                        			</nav>
+                        		</div>
+                        	</div>
+                        </div>
                     </div>
                 </div>
             </article>
