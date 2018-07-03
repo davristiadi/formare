72301cd7dd32f79e8271fa3518c0fee37bba9cf2
David Kurnia Kristiadi <davidkurniakristiadi@gmail.com>
2018-07-03 16:40:04 +0700

    Adjusted opacity in hero--has-overlay class modifier

--- docs/src/views/Global/Home/Home.js
+++ docs/src/views/Global/Home/Home.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Home = props => {
 	return (
 		<div id="home">
-			<section className="hero hero--primary hero--has-overlay" style={{backgroundImage: `url('https://images.unsplash.com/flagged/photo-1530447920184-b8a8c88ee72a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3511890e01f7243a75369ac74f67026&auto=format&fit=crop&w=700&q=80')`}}>
-				<div className="container pd-top-large">
-					<h2 className="title">Formare is lightweight, fast and easy to get started with. </h2>
-					<h4 className="subtitle color-gray">Veniam dolore adipisicing eu excepteur aliqua deserunt laboris dolor dolor commodo nisi occaecat aute pariatur sunt culpa mollit ullamco.</h4>
+			<section
+				className="hero hero--darkest hero--has-overlay hero--absolute-center"
+				style={{
+					backgroundImage: `url('https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7ca95b1290dfb28db0ee368546e55487&auto=format&fit=crop&w=1050&q=80')`
+				}}
+			>
+				<div className="container pd-top-large flex ai-center ta-center">
+					<div className="heading">
+						<h2 className="title">
+							Formare is lightweight, fast and easy to get started
+							with.{" "}
+						</h2>
+						<h4 className="subtitle color-light">
+							Veniam dolore adipisicing eu excepteur aliqua deserunt
+							laboris dolor dolor commodo nisi occaecat aute pariatur
+							sunt culpa mollit ullamco.
+						</h4>
+					</div>
 				</div>
 			</section>
 		</div>
-	)
-}
+	);
+};
 
 export default Home;
