9d262db585e31c7b9899fb16b7cfc3a4114ea4f8
davidkurniakristiadi@gmail.com <davidkurniakristiadi@Davids-MacBook-Pro.local>
2018-09-07 00:59:04 +0700

    Finalized Layout system inspired by ant design's layout system.

--- docs/src/views/Global/Docs/Docs.js
+++ docs/src/views/Global/Docs/Docs.js
@@ -7,8 +7,20 @@ import DocsMenu from './DocsMenu';
 
 const Docs = props => {
 	return (
-		<div className="fr-docs">
-			<div className="container">
+		<div className="fr-layout">
+			<div className="container flex">
+				<main className="fr-layout pd-top-large pd-bottom pd-right-large">
+					<Switch>
+						<Route exact path="/docs" component={DocsHome} />
+						<Route path="/docs/components" component={Components} />
+						<Route path="/docs/forms" component={Forms} />
+					</Switch>
+				</main>
+				<aside className="fr-aside">
+					<DocsMenu />
+				</aside>
+			</div>
+			{/* <div className="container">
 				<div className="grids">
 					<main className="fr-docs__body grid">
 						<Switch>
@@ -21,14 +33,14 @@ const Docs = props => {
 						<DocsMenu />
 					</aside>
 				</div>
-			</div>
+			</div> */}
 		</div>
 	);
 }
 
 const DocsHome = props => {
 	return (
-		<div id="docs-home">
+		<div>
 			<article className="fr-article article">
 				<div className="fr-heading heading">
 					<h3 className="title">Documentation</h3>
