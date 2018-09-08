7efdb7e32c592787a86ac61ff00a013d512cab1c
davidkurniakristiadi@gmail.com <davidkurniakristiadi@Davids-MacBook-Pro.local>
2018-09-09 04:33:51 +0700

    Completely revamped the color system in the framework. Removed theme color shades, but shades of gray remains.

--- docs/src/views/Global/Docs/Components/Navbar/Navbar.js
+++ docs/src/views/Global/Docs/Components/Navbar/Navbar.js
@@ -2,16 +2,16 @@ import React, { PropTypes } from 'react'
 
 const Navbar = props => {
     return (
-        <div id="navbar">
-            <article class="article">
-                <div class="fr-heading heading">
-                    <h3 class="title">Navbar</h3>
-                    <h5 class="subtitle color-gray">Consectetur non Lorem mollit incididunt nostrud qui. Tempor ea enim qui in id mollit anim consectetur id sit aute id proident. Tempor Lorem ea dolor cupidatat nulla commodo amet.</h5>
+        <div className="fr-navbar-page">
+            <article className="article">
+                <div className="fr-heading heading">
+                    <h3 className="title">Navbar</h3>
+                    <h5 className="subtitle color-gray">Consectetur non Lorem mollit incididunt nostrud qui. Tempor ea enim qui in id mollit anim consectetur id sit aute id proident. Tempor Lorem ea dolor cupidatat nulla commodo amet.</h5>
                 </div>
-                <div class="fr-preview">
-                    <div class="fr-preview__tag">Preview</div>
-                    <div class="fr-preview__display">
-                        <div className="navbar mg-bottom">
+                <div className="fr-preview">
+                    <div className="fr-preview__tag">Preview</div>
+                    <div className="fr-preview__display">
+                        <div className="navbar navbar--secondary">
                         	<div className="navbar__container navbar__container--full-width">
                         		<div className="navbar__block">
                         			<h5>Logo</h5>
