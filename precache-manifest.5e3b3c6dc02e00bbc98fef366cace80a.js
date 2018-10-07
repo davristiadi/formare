dc6cded441399a05b3a9a4c6b4967bfa8a9fd42b
davidkurniakristiadi@gmail.com <davidkurniakristiadi@Davids-MacBook-Pro.local>
2018-10-07 17:43:50 +0700

    Updates

---
+++ precache-manifest.5e3b3c6dc02e00bbc98fef366cace80a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f621bb7b7f1ccd6ecea8410dc1a5088",
+    "url": "/formare/static/media/dummy-photo.8f621bb7.jpeg"
+  },
+  {
+    "revision": "271a316daee0d65335d9",
+    "url": "/formare/static/js/runtime~main.271a316d.js"
+  },
+  {
+    "revision": "c599a13fb368ef003f7a",
+    "url": "/formare/static/js/main.c599a13f.chunk.js"
+  },
+  {
+    "revision": "0aba23539d18ccc15326",
+    "url": "/formare/static/js/1.0aba2353.chunk.js"
+  },
+  {
+    "revision": "c599a13fb368ef003f7a",
+    "url": "/formare/static/css/main.a5642507.chunk.css"
+  },
+  {
+    "revision": "f7cdfc90b8b9405a3f91dc161c0cacf4",
+    "url": "/formare/index.html"
+  }
+];
