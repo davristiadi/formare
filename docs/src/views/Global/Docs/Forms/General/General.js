b976d09bed472673c4478258b325666e5ce59354
dkk94 <davidkurniakristiadi@gmail.com>
2018-10-17 01:22:55 +0700

    Small adjustment commit

--- docs/src/views/Global/Docs/Forms/General/General.js
+++ docs/src/views/Global/Docs/Forms/General/General.js
@@ -50,7 +50,7 @@ const General = props => {
                             </div>
                             <div className="form__field">
                                 <label className="form__label">Website</label>
-                                <div className="form__input form__input--grouped">
+                                <div className="form__input">
                                     <div className="select">
                                         <select>
                                             <option>https://</option>
@@ -85,16 +85,38 @@ const General = props => {
                             </div>
                             <div class="form__field">
                                 <label class="form__label">Phone</label>
-                                <div class="input-set">
-                                    <div class="input-set__extra">
-                                        <i class="icon fas fa-phone" />
+                                <div class="form__input">
+                                    <div class="input-set">
+                                        <div class="input-set__extra">
+                                            <i class="icon fas fa-phone" />
+                                        </div>
+                                        <input type="text" class="input" placeholder="e.g +62 812 345 678" />
                                     </div>
-                                    <input type="text" class="input" placeholder="e.g +62 812 345 678" />
                                 </div>
                             </div>
                             <div class="form__field">
                                 <label class="form__label">Address</label>
-                                <textarea class="textarea" rows="3" placeholder="e.g Bryar Pitts, 5543 Aliquet St. Fort Dodge GA 20783, (717) 450-4729"></textarea>
+                                <div class="form__input">
+                                    <textarea class="textarea" rows="3" placeholder="e.g Bryar Pitts, 5543 Aliquet St. Fort Dodge GA 20783, (717) 450-4729"></textarea>
+                                </div>
+                            </div>
+                            <div class="form__field">
+                                <label class="form__label">Website</label>
+                                <div class="form__input">
+                                    <div class="select">
+                                        <select>
+                                            <option>https://</option>
+                                            <option>http://</option>
+                                        </select>
+                                    </div>
+                                    <input type="text" class="input" placeholder="e.g formare.com" />
+                                    <div class="select">
+                                        <select>
+                                            <option>.com</option>
+                                            <option>.co.id</option>
+                                        </select>
+                                    </div>
+                                </div>
                             </div>
                         </form>
                         `}
