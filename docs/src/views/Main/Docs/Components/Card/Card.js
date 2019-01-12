e4d5e682fe1c41f55253b4bfcd4a4abcfc2c088b
dkk94 <david.kurnia@amarbank.co.id>
2019-01-12 20:49:57 +0700

    Added themes support in Card component.

--- docs/src/views/Main/Docs/Components/Card/Card.js
+++ docs/src/views/Main/Docs/Components/Card/Card.js
@@ -18,80 +18,92 @@ const Card = props => {
           <h4 className="Title">Overview</h4>
         </div>
         <p>Pariatur pariatur consectetur qui ullamco Lorem amet ut officia culpa.</p>
-        <Preview>
+        <Preview borderless>
           <PreviewDisplay>
-            <div className="Grids">
-              <div className="Grid Grid-6">
-                <div className="Card">
-                  <div className="Card-header">
-                    <h5 className="Title">Card Title</h5>
-                    <h6 className="subtitle color-gray">This is a subtitle</h6>
-                  </div>
-                  <div className="Card-body">
-                    <p>
-                      Elit non consectetur eu dolor deserunt eiusmod deserunt. 
-                      Eu fugiat esse aliqua magna excepteur irure minim. Id tempor esse cillum aliquip enim.
-                    </p>
-                  </div>
-                  <div className="Card-footer">
-                    <button className="Button Button--primary">Submit</button>
-                  </div>
-                </div>
+            <div className="Card" style={{maxWidth: '400px'}}>
+              <div className="Card-header">
+                <h5 className="Title">Card Title</h5>
+                <h6 className="subtitle color-gray">This is a subtitle</h6>
               </div>
-              <div className="Grid Grid-6">
-                <div className="Card">
-                  <div className="Card-header">
-                    <h5 className="Title">Card Title</h5>
-                    <h6 className="subtitle color-gray">This is a subtitle</h6>
-                  </div>
-                  <div className="Card-body">
-                    <span>
-                      Elit non consectetur eu dolor deserunt eiusmod deserunt. 
-                      Eu fugiat esse aliqua magna excepteur irure minim. Id tempor esse cillum aliquip enim.
-                    </span>
-                  </div>
-                  <div className="Card-footer">
-                    <button className="Button Button--primary">Submit</button>
-                  </div>
-                </div>
+              <div className="Card-body">
+                <p>Ut eiusmod magna laboris deserunt sunt duis et id adipisicing et elit. 
+                  Tempor ipsum consectetur enim nisi tempor culpa ex aliquip ad proident.
+                  Velit sint ad sit sint sint et aliqua consequat commodo est laboris enim consectetur.
+                </p>
+              </div>
+              <div className="Card-footer">
+                <button className="Button Button--primary">Submit</button>
               </div>
             </div>
           </PreviewDisplay>
-          <PreviewHighlight lang="html">
-            {`<div className="Card">
-                <div className="Card-header">
-                  <h5 className="Title">Card Title</h5>
-                  <h6 className="subtitle color-gray">This is a subtitle</h6>
-                </div>
-                <div className="Card-body">
-                  <span>
-                    Elit non consectetur eu dolor deserunt eiusmod deserunt. 
-                    Eu fugiat esse aliqua magna excepteur irure minim. Id tempor esse cillum aliquip enim.
-                  </span>
-                </div>
-                <div className="Card-footer">
-                  <button className="button button--primary">Submit</button>
-                </div>
-              </div>
-            `}
-          </PreviewHighlight>
         </Preview>
       </section>
 
 
       <section id="themes" className="fr-DocsArticle-section">
-        <article className="article">
-          <div className="Heading">
-            <h4 className="Title">Themes</h4>
-          </div>
-          <p>Dolore est excepteur culpa voluptate culpa quis dolor 
-            ipsum et et exercitation.</p>
-        </article>
+        <div className="Heading">
+          <h4 className="Title">Themes</h4>
+        </div>
+        <p>Dolore est excepteur culpa voluptate culpa quis dolor 
+          ipsum et et exercitation.</p>
+        <Preview>
+          <PreviewDisplay>
+            <div className="fr-CardExample">
+              <div className="Grids">
+                <div className="Grid Grid-6 u-MarginBottom-base">
+                  <div className="Card Card--primary">
+                    <div className="Card-header">
+                      <h5 className="Title">Primary Theme</h5>
+                    </div>
+                    <div className="Card-body">
+                      <div>
+                        Cupidatat ut esse eiusmod pariatur. Voluptate culpa eu reprehenderit eu voluptate sit ad nisi. 
+                        Minim pariatur aliqua aliqua adipisicing mollit laborum Lorem. Quis ipsum velit et nulla Lorem enim 
+                        aliquip exercitation ad elit ad reprehenderit. Voluptate minim duis incididunt elit amet velit dolor. 
+                        Sunt occaecat dolore irure magna sit incididunt anim consequat consequat.
+                      </div>
+                    </div>
+                    <div className="Card-footer">
+                      <button className="Button Button--light Button--outlined">Submit</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="Grid Grid-6 u-MarginBottom-base">
+                  <div className="Card Card--dark">
+                    <div className="Card-header">
+                      <h5 className="Title">Secondary Theme</h5>
+                    </div>
+                    <div className="Card-body">
+                      <div>
+                        Cupidatat ut esse eiusmod pariatur. Voluptate culpa eu reprehenderit eu voluptate sit ad nisi. 
+                        Minim pariatur aliqua aliqua adipisicing mollit laborum Lorem. Quis ipsum velit et nulla Lorem enim 
+                        aliquip exercitation ad elit ad reprehenderit. Voluptate minim duis incididunt elit amet velit dolor. 
+                        Sunt occaecat dolore irure magna sit incididunt anim consequat consequat.
+                      </div>
+                    </div>
+                    <div className="Card-footer">
+                      <button className="Button Button--primary">Submit</button>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+          </PreviewDisplay>
+        </Preview>
       </section>
 
-      <section id="card-with-thumbnail" className="fr-DocsArticle-section">
+      <section id="images" className="fr-DocsArticle-section">
         <div className="Heading">
-          <h4 className="Title">Card with Thumbnail</h4>
+          <h4 className="Title">Images</h4>
+        </div>
+        <p>Images can also be included inside of a card to support the piece of information you are trying to convey to your users.
+          There a few out-of-the-box options you can choose, including adding image thumbnail to a certain part of your
+          card, overlaying your image into your card.
+        </p>
+
+        <div className="Heading">
+          <h5 className="Title">Thumbnail</h5>
         </div>
         <p>
           Minim fugiat ad aliquip aute magna in aliquip cupidatat incididunt.
@@ -101,170 +113,87 @@ const Card = props => {
         <Preview>
           <PreviewDisplay>
             <div className="Grids">
-              <div className="grid grid-6">
+              <div className="Grid Grid-6">
                 <div className="Card">
                   <div className="Card-image">
                     <img src={PhotoDummy} />
                   </div>
                   <div className="Card-body">
                     <h5 className="Title">Card Title</h5>
-                    <h6 className="subtitle color-gray">This is a subtitle.</h6>
+                    <h6 className="Subtitle u-Color-gray">This is a subtitle.</h6>
                     <p>
-                      Tempor eiusmod excepteur esse culpa qui elit sint aliqua
-                      dolor. Qui mollit esse officia labore deserunt laborum
-                      dolore. Adipisicing mollit dolore eiusmod aute pariatur
-                      nulla culpa laborum culpa et dolore aute aute. Aute
-                      voluptate excepteur velit proident laboris qui.
+                      Anim aliquip esse dolore proident ut ut anim. Ut dolore proident tempor proident sunt irure.
+                      Est excepteur anim aute sint dolor irure occaecat cillum tempor eu anim Lorem ullamco.
+                      Tempor sit ad consequat nostrud sunt pariatur.
                     </p>
                   </div>
                   <div className="Card-footer">
-                    <button className="button button--primary">Submit</button>
+                    <button className="Button Button--primary">Submit</button>
                   </div>
                 </div>
               </div>
-              <div className="grid grid-6">
+              <div className="Grid Grid-6">
                 <div className="Card">
                   <div className="Card-header">
                     <h5 className="Title">Card Title</h5>
-                    <h6 className="subtitle color-gray">This is a subtitle. </h6>
+                    <h6 className="Subtitle u-Color-gray">This is a subtitle. </h6>
                   </div>
                   <div className="Card-image">
                     <img src={PhotoDummy} />  
                   </div>
                   <div className="Card-body">
-                    Culpa ea reprehenderit Lorem ad voluptate. Ad voluptate eu cillum reprehenderit. 
-                    Nisi sit laborum dolore exercitation.
+                  <p>Lorem excepteur qui anim sunt ex id voluptate amet. Sint eiusmod tempor eiusmod ea dolore dolor minim esse laborum do.</p>
                   </div>
                   <div className="Card-footer">
-                    <button className="button button--primary">Submit</button>
+                    <button className="Button Button--primary">Submit</button>
                   </div>
                 </div>
               </div>
             </div>
           </PreviewDisplay>
-          <PreviewHighlight lang="html">
-            {`<div className="Card">
-              <div className="Card-image">
-                <img src="path/to/image" />
-              </div>
-              <div className="Card-body">
-                <h5 className="Title">Card Title</h5>
-                <h6 className="subtitle color-gray">This is a subtitle.</h6>
-                <p>
-                  Tempor eiusmod excepteur esse culpa qui elit sint aliqua
-                  dolor. Qui mollit esse officia labore deserunt laborum
-                  dolore. Adipisicing mollit dolore eiusmod aute pariatur
-                  nulla culpa laborum culpa et dolore aute aute. Aute
-                  voluptate excepteur velit proident laboris qui.
-                </p>
-              </div>
-              <div className="Card-footer">
-                <button className="button button--primary">Submit</button>
-              </div>
-            </div>
-            `}
-          </PreviewHighlight>
-        </Preview>
-      </section>
-
-      <section id="overlay" className="fr-DocsArticle-section">
-        <article className="article">
-          <div className="Heading">
-            <h4 className="Title">Card with Image Overlay</h4>
-          </div>
-          <p>Est anim in in fugiat mollit sunt consectetur cupidatat deserunt.</p>
-          <Preview>
-            <PreviewDisplay>
-              <div className="card card--dark card--has-overlay" style={{maxWidth: '400px'}}>
-                <div className="card-image">
-                  <img src={PhotoDummy} />
-                </div>
-                <div className="card-body">
-                  <h5 class="title">Lorem ea non cillum.</h5>
-                  <span>
-                    Cupidatat duis proident exercitation ullamco sunt
-                    labore.
-                  </span>
-                </div>
-              </div>
-            </PreviewDisplay>
-            <PreviewHighlight lang="html">
-              {`<div class="card card--dark card--has-overlay">
-                <div class="card-image">
-                  <img src="path/to/image" />
-                </div>
-                <div class="card-body">
-                  <h5 class="title">Lorem ea non cillum.</h5>
-                  <span>
-                    Cupidatat duis proident exercitation ullamco sunt
-                    labore.
-                  </span>
-                </div>
-              </div>
-              `}
-            </PreviewHighlight>
-          </Preview>
           
-          <div className="fr-preview">
-            <span className="fr-preview-tag">Preview</span>
-            <div className="fr-preview-display">
+        </Preview>
+        
+        <div className="Heading">
+          <h5 className="Title">Overlay</h5>
+        </div>
+        <p>Est anim in in fugiat mollit sunt consectetur cupidatat deserunt.</p>
+        <Preview>
+          <PreviewDisplay>
+            <div className="fr-CardExample">
               <div className="Grids">
-                <div className="grid grid-6 u-MarginBottom">
-                  <div className="card card--dark card--has-overlay">
-                    <div className="card-image">
+                <div className="Grid Grid-6">
+                  <div className="Card Card--dark Card--hasOverlay">
+                    <div className="Card-image">
                       <img src={PhotoDummy} />
                     </div>
-                    <div className="card-body">
-                      <h5 class="title">Lorem ea non cillum.</h5>
-                      <span>
-                        Cupidatat duis proident exercitation ullamco sunt
-                        labore.
-                      </span>
+                    <div className="Card-body">
+                      <h5 class="Title">Lorem ea non cillum.</h5>
+                      <p>Excepteur veniam dolor adipisicing ex sit. Dolore est laborum aute commodo nisi aliquip pariatur eu non magna.
+                        Mollit amet laboris officia velit officia et ea enim ea ut quis velit officia velit.
+                        Quis fugiat incididunt ad excepteur anim veniam ipsum minim nisi veniam velit ipsum do.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="Grid Grid-6">
+                  <div className="Card Card--primary Card--hasOverlay">
+                    <div className="Card-image">
+                      <img src={PhotoDummy} />
+                    </div>
+                    <div className="Card-body">
+                      <h5 class="Title">Lorem ea non cillum.</h5>
+                      <p>Excepteur veniam dolor adipisicing ex sit. Dolore est laborum aute commodo nisi aliquip pariatur eu non magna.
+                        Mollit amet laboris officia velit officia et ea enim ea ut quis velit officia velit.
+                        Quis fugiat incididunt ad excepteur anim veniam ipsum minim nisi veniam velit ipsum do.
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </article>
-      </section>
-      <section className="section">
-        <article className="article">
-          <div className="Heading">
-            <h4 className="Title">Borderless Card</h4>
-            <h6 className="subtitle color-gray">
-              Eu duis irure proident pariatur est commodo eu veniam ut.
-            </h6>
-          </div>
-          <div className="fr-preview">
-            <span className="fr-preview-tag">Preview</span>
-            <div className="fr-preview-display">
-              <div className="Grids">
-                <div className="grid grid-6 u-MarginBottom">
-                  <div className="card card--transparent">
-                    <div className="card-image">
-                      <img src={PhotoDummy} />
-                    </div>
-                    <div className="card-body">
-                      <article className="article">
-                        <div className="Heading">
-                          <h5 className="Title">This is an example card</h5>
-                          <h6 className="subtitle color-gray">
-                            Tempor ad adipisicing fugiat labore et exercitation.
-                          </h6>
-                        </div>
-                        <p>
-                          Quis quis mollit ipsum sint aliquip occaecat veniam
-                          nulla do incididunt non ipsum consectetur duis.
-                        </p>
-                      </article>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </article>
+          </PreviewDisplay>
+        </Preview>
       </section>
     </article>
   );
