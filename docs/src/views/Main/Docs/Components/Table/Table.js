ee7c7e542e4030611706730b0060f759b22b2a26
dkk94 <david.kurnia@amarbank.co.id>
2019-02-12 16:46:26 +0700

    Implemented the new naming convention on the Table component. Updated Table docs with the latest update to Table class.

--- docs/src/views/Main/Docs/Components/Table/Table.js
+++ docs/src/views/Main/Docs/Components/Table/Table.js
@@ -1,111 +1,119 @@
-import React, { PropTypes } from 'react'
-import { Highlight } from 'components/Highlight';
+import React, { PropTypes } from 'react';
+import { Preview, PreviewDisplay, PreviewHighlight } from 'components/Preview';
 
 const Table = props => {
-    return (
-        <div id="card">
-            <article className="fr-DocsArticle article">
-                <div className="fr-heading heading">
-                    <h2 className="Title">Table</h2>
-                </div>
-                <section>
-                    <div className="table table--striped u-MarginBottom">
-                        <table>
-                            <thead className="table__head">
-                                <tr>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Height</th>
-                                    <th>Birth Year</th>
-                                    <th>Gender</th>
-                                </tr>
-                            </thead>
-                            <tbody className="table__body">
-                                <tr>
-                                    <td>1</td>
-                                    <td>Luke Skywalker</td>
-                                    <td>172 cm</td>
-                                    <td>19BBY</td>
-                                    <td>Male</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>C-3PO</td>
-                                    <td>167</td>
-                                    <td>112BBY</td>
-                                    <td>N/A</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>R2-D2</td>
-                                    <td>96</td>
-                                    <td>33BBY</td>
-                                    <td>N/A</td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>Darth Vader</td>
-                                    <td>202</td>
-                                    <td>41.9BBY</td>
-                                    <td>Male</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <Highlight lang="html">
-                        {`<div class="table">
-                            <table>
-                                <thead className="table__head">
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Name</th>
-                                        <th>Height</th>
-                                        <th>Birth Year</th>
-                                        <th>Gender</th>
-                                    </tr>
-                                </thead>
-                                <tbody className="table__body">
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Luke Skywalker</td>
-                                        <td>172 cm</td>
-                                        <td>19BBY</td>
-                                        <td>Male</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>C-3PO</td>
-                                        <td>167</td>
-                                        <td>112BBY</td>
-                                        <td>N/A</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>R2-D2</td>
-                                        <td>96</td>
-                                        <td>33BBY</td>
-                                        <td>N/A</td>
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td>Darth Vader</td>
-                                        <td>202</td>
-                                        <td>41.9BBY</td>
-                                        <td>Male</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>`}
-                    </Highlight>
-                </section>
-            </article>
+  return (
+    <div id="card">
+      <article className="fr-DocsArticle">
+        <div className="fr-DocsArticle-heading">
+          <h2 className="fr-DocsArticle-title">Table</h2>
+          <h5 className="fr-DocsArticle-subtitle">Officia ullamco sit aliqua consectetur commodo tempor magna fugiat aliqua cillum.</h5>
         </div>
-    )
-}
+        <section className="fr-DocsArticle-section">
+          <div className="Heading">
+            <h4 className="Title">Overview</h4>
+          </div>
+          <p>Nostrud sint id velit ipsum nulla irure labore quis. Aliqua cillum non pariatur ad est non.</p>
+          <Preview>
+            <PreviewDisplay>
+              <div className="Table">
+                <table>
+                  <thead className="Table-head">
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Height</th>
+                      <th>Birth Year</th>
+                      <th>Gender</th>
+                    </tr>
+                  </thead>
+                  <tbody className="Table-body">
+                    <tr>
+                      <td>1</td>
+                      <td>Luke Skywalker</td>
+                      <td>172 cm</td>
+                      <td>19BBY</td>
+                      <td>Male</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>C-3PO</td>
+                      <td>167 cm</td>
+                      <td>112BBY</td>
+                      <td>N/A</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>R2-D2</td>
+                      <td>96 cm</td>
+                      <td>33BBY</td>
+                      <td>N/A</td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Darth Vader</td>
+                      <td>202 cm</td>
+                      <td>41.9BBY</td>
+                      <td>Male</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </PreviewDisplay>
+            <PreviewHighlight lang="html">
+              {`
+              <div className="Table table--striped">
+                <table>
+                  <thead className="Table-head">
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Height</th>
+                      <th>Birth Year</th>
+                      <th>Gender</th>
+                    </tr>
+                  </thead>
+                  <tbody className="Table-body">
+                    <tr>
+                      <td>1</td>
+                      <td>Luke Skywalker</td>
+                      <td>172 cm</td>
+                      <td>19BBY</td>
+                      <td>Male</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>C-3PO</td>
+                      <td>167</td>
+                      <td>112BBY</td>
+                      <td>N/A</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>R2-D2</td>
+                      <td>96</td>
+                      <td>33BBY</td>
+                      <td>N/A</td>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Darth Vader</td>
+                      <td>202</td>
+                      <td>41.9BBY</td>
+                      <td>Male</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              `}
+            </PreviewHighlight>
+          </Preview>
+        </section>
+      </article>
+    </div>
+  );
+};
 
-Table.propTypes = {
-    
-}
+Table.propTypes = {};
 
-export default Table
+export default Table;
