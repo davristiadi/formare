d8c0a0780d1975e048a71fa65c080dba9bad8722
dkk94 <david.kurnia@amarbank.co.id>
2019-03-13 15:01:13 +0700

    Replaced select input arrow implementation from using Font Awesome to custom made

--- docs/src/views/Main/Docs/Forms/Inputs/Inputs.js
+++ docs/src/views/Main/Docs/Forms/Inputs/Inputs.js
@@ -15,12 +15,32 @@ const Inputs = () => {
         <p>Ad elit aliquip aliquip amet duis esse minim irure aliqua Lorem cupidatat sit irure fugiat.</p>
         <Preview layout="horizontal">
           <PreviewDisplay>
-            <input className="Input" type="text" placeholder="This is an input" />
+            <input className="Input u-mg-bottom" type="text" placeholder="This is an input" />
+            <div className="Select">
+              <select>
+                <option>This is a select input</option>
+                <option>Option 1</option>
+                <option>Option 2</option>
+                <option>Option 3</option>
+              </select>
+            </div>
           </PreviewDisplay>
           <PreviewHighlight lang="html">
-            {`<input class="Input" type="text" placeholder="This is an input" />`}
+            {`
+            <input class="Input" type="text" placeholder="This is an input" />
+            <div className="Select">
+              <select>
+                <option>This is a select input</option>
+                <option>Option 1</option>
+                <option>Option 2</option>
+                <option>Option 3</option>
+              </select>
+            </div>`}
           </PreviewHighlight>
         </Preview>
+      </section>
+      <section className="fr-DocsArticle-section">
+      
       </section>
     </article>
   );
