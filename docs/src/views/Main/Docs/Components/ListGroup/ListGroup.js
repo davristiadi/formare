1fa90a7c89681c30363c4445bdb56ee2da16782a
dkk94 <davidkurniakristiadi@gmail.com>
2019-03-19 19:53:26 +0700

    Added borderless modifier to ListGroup class

--- docs/src/views/Main/Docs/Components/ListGroup/ListGroup.js
+++ docs/src/views/Main/Docs/Components/ListGroup/ListGroup.js
@@ -1,38 +1,35 @@
-import React from 'react';
-import classNames from 'classnames';
-import { 
-  Preview,
-  PreviewDisplay,
-  PreviewHighlight
-} from 'components/Preview';
+import React from "react";
+import classNames from "classnames";
+import { Preview, PreviewDisplay, PreviewHighlight } from "components/Preview";
 
 const ListGroup = () => {
   return (
     <article id="list-group" className="fr-DocsArticle">
       <div className="fr-DocsArticle-heading">
         <h2 className="fr-DocsArticle-title">List Group</h2>
-        <h5 className="fr-DocsArticle-subtitle">Nulla eiusmod veniam excepteur do anim Lorem et dolore incididunt amet consequat cillum.</h5>
+        <h5 className="fr-DocsArticle-subtitle">
+          Nulla eiusmod veniam excepteur do anim Lorem et dolore incididunt amet consequat cillum.
+        </h5>
       </div>
       <section className="fr-DocsArticle-section">
         <div className="Heading">
           <h4 className="Title">Overview</h4>
         </div>
-        <p>You can use a list group component to sort out a related content about a subject in a form of a list.</p>
+        <p>
+          You can use a list group component to sort out a related content about a subject in a form
+          of a list.
+        </p>
         <Preview layout="horizontal">
           <PreviewDisplay>
-            <ul className="ListGroup">
-              <li className="ListGroup-item">
-                Adipisicing in fugiat amet anim proident.
-              </li>
+            <ul className="ListGroup ListGroup--borderless">
+              <li className="ListGroup-item">Adipisicing in fugiat amet anim proident.</li>
               <li className="ListGroup-item">
                 Dolor duis aute laborum Lorem qui exercitation esse.
               </li>
               <li className="ListGroup-item">
                 Eu excepteur do incididunt occaecat consequat mollit reprehenderit.
               </li>
-              <li className="ListGroup-item">
-                Aliquip est nisi non sit nostrud pariatur est.
-              </li>
+              <li className="ListGroup-item">Aliquip est nisi non sit nostrud pariatur est.</li>
             </ul>
           </PreviewDisplay>
           <PreviewHighlight lang="html">
