58642e8bf589df0fb6d06181c21b4eaeefd64d45
dkk94 <davidkurniakristiadi@gmail.com>
2020-01-04 23:26:30 +0700

    Improve modal backdrop opacity in fr-modal

--- packages/fr-playground/src/pages/modal.js
+++ packages/fr-playground/src/pages/modal.js
@@ -2,34 +2,36 @@ import React from 'react';
 
 const Modal = () => {
   return (
-    <div className="Modal">
-      <div className="Modal-content">
-        <div className="Modal-block">
-          <h1 className="Heading-2">Modal Title</h1>
-          <p className="Heading-5 Color-gray">Qui ut duis velit do cupidatat officia deserunt.</p>
+    <>
+      <div className="Modal">
+        <div className="Modal-content">
+          <div className="Modal-block">
+            <h1 className="Heading-2">Modal Title</h1>
+            <p className="Heading-5 Color-gray">Qui ut duis velit do cupidatat officia deserunt.</p>
+          </div>
+          <div className="Modal-block FlexGrow-1">
+            <p>
+              Nisi cillum cillum exercitation dolor minim elit adipisicing cupidatat irure amet
+              velit id. Tempor laboris mollit nisi fugiat duis nulla et mollit commodo. Velit
+              consectetur consectetur nisi sunt excepteur magna cillum ea cupidatat eiusmod nisi
+              sint. Deserunt aute enim cupidatat mollit duis voluptate Lorem cillum tempor cillum
+              occaecat duis enim sunt. Voluptate sint irure est eiusmod. Veniam nisi fugiat esse
+              quis non ex adipisicing sint irure dolor.
+            </p>
+            <p>
+              Consectetur incididunt eiusmod cillum officia Lorem dolor quis exercitation cupidatat
+              sint ut. Irure anim ut amet proident ullamco fugiat cillum dolor nisi duis mollit est.
+              Sint sunt esse enim est exercitation Lorem culpa et reprehenderit.
+            </p>
+          </div>
+          <div className="Modal-block Flex JustifyContent-between">
+            <button className="Button Button--danger Button--elevated">Decline</button>
+            <button className="Button Button--primary Button--elevated">Accept</button>
+          </div>
         </div>
-        <div className="Modal-block FlexGrow-1">
-          <p>
-            Nisi cillum cillum exercitation dolor minim elit adipisicing cupidatat irure amet velit
-            id. Tempor laboris mollit nisi fugiat duis nulla et mollit commodo. Velit consectetur
-            consectetur nisi sunt excepteur magna cillum ea cupidatat eiusmod nisi sint. Deserunt
-            aute enim cupidatat mollit duis voluptate Lorem cillum tempor cillum occaecat duis enim
-            sunt. Voluptate sint irure est eiusmod. Veniam nisi fugiat esse quis non ex adipisicing
-            sint irure dolor.
-          </p>
-          <p>
-            Consectetur incididunt eiusmod cillum officia Lorem dolor quis exercitation cupidatat
-            sint ut. Irure anim ut amet proident ullamco fugiat cillum dolor nisi duis mollit est.
-            Sint sunt esse enim est exercitation Lorem culpa et reprehenderit.
-          </p>
-        </div>
-        <div className="Modal-block Flex JustifyContent-between">
-          <button className="Button Button--danger Button--elevated">Decline</button>
-          <button className="Button Button--primary Button--elevated">Accept</button>
-        </div>
+        <div className="Modal-backdrop"></div>
       </div>
-      <div className="Modal-backdrop"></div>
-    </div>
+    </>
   );
 };
 
